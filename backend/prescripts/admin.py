7090c7aea1b43ce110daf07daa5c9470522013fc
NadezdaMamaeva <pno05@mail.ru>
2023-06-20 12:35:56 +0500

    add prescriptors model

--- backend/prescripts/admin.py
+++ backend/prescripts/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (Tag, Prescriptor,)
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('slug', 'name', 'color',)
+    list_filter = ('color',)
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',), }
+
+
+class PrescriptorAdmin(admin.ModelAdmin):
+    list_display = ('author', 'name', 'get_tags',)
+    list_display_links = ('author',)
+    list_editable = ('name',)
+    list_filter = ('author', 'name', 'tags',)
+    empty_value_display = '-'
+    search_fields = ('name',)
+
+    @admin.display(description='Теги')
+    def get_tags(self, obj):
+        return ','.join([tag.name for tag in obj.tags.all()])
+
+
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Prescriptor, PrescriptorAdmin)
--- backend/prescripts/models.py
+++ backend/prescripts/models.py
@@ -1,3 +1,54 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Tag(models.Model):
+    slug = models.SlugField(max_length=64, unique=True,
+                            verbose_name='Ссылка',)
+    name = models.CharField(max_length=64, unique=True,
+                            verbose_name='Имя',)
+    color = models.CharField(max_length=16, verbose_name='Цвет',)
+
+    class Meta:
+        ordering = ('name', 'color',)
+        constraints = (models.UniqueConstraint(
+                       fields=('name', 'color'),
+                       name='unique_tag'),)
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+
+
+class Prescriptor(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='prescriptors',
+        verbose_name='Автор публикации',)
+    name = models.CharField(max_length=100, verbose_name='Название рецепта',)
+    image = models.ImageField(
+        upload_to='prescriptors/images/', null=True, blank=True,
+        verbose_name='Изображение рецепта', default=None)
+    text = models.TextField(verbose_name='Описание рецепта',)
+    tags = models.ManyToManyField(Tag, related_name='prescriptors',
+                                  verbose_name='Теги',)
+    cooking_time = models.PositiveSmallIntegerField(
+        verbose_name='Время приготовления рецепта',)
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True,)
+
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return f'{self.name} от {self.author}'
